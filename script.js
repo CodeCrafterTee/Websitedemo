f58c08189c3f45976e05d488acff61ec908f06ee
CodeCrafterTee <lateefa.mmadi@gmail.com>
2025-04-03 11:51:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to toggle menu
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+// Function to toggle cart
+function toggleCart() {
+    var cart = document.getElementById("cart");
+    if (cart.style.display === "block") {
+        cart.style.display = "none";
+    } else {
+        cart.style.display = "block";
+    }
+}
